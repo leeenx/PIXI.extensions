b1ad99846a61f070327bfa5153ebd5ee03dd1635
leeenx <leeenx@users.noreply.github.com>
2017-10-17 03:04:18 -0500

    PIXI的两个扩展。让 PIXI 用起来更像 createjs

---
+++ extensions.js
@@ -0,0 +1,26 @@
+// PIXI 的扩展
+PIXI.Sprite.prototype.set = function(arg) {
+	for(let key in arg) {
+		this[key] = arg[key]; 
+	}
+}
+
+// scale 属性拍平
+Object.defineProperties(PIXI.DisplayObject.prototype, {
+	scaleX: {
+		set: function(value) {
+			this.scale.x = value; 
+		}, 
+		get: function() {
+			return this.scale.x; 
+		}
+	}, 
+	scaleY: {
+		set: function(value) {
+			this.scale.y = value; 
+		}, 
+		get: function() {
+			return this.scale.y; 
+		}
+	}
+}); 
